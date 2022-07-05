72011a791ed57ac288ccb1e400b05f0e089b0637
Taekook-x-Drarry <99473581+Taekook-x-Drarry@users.noreply.github.com>
2022-07-05 19:05:13 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,46 @@
+noseX = 0;
+noseY = 0;
+
+function preload()
+{
+    clown_nose = loadImage('https://i.postimg.cc/cJTr1Phz/l1.png');
+}
+
+function setup()
+{
+    canvas = createCanvas(300 , 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    video.size(300 , 300);
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose' , gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('poseNet Is Initialized');
+}
+
+function draw()
+{
+    image(video , 0 , 0 , 300 , 300);
+    image(clown_nose , noseX , noseY , 30 , 30);
+}
+
+function take_snapshot()
+{
+    save('myFilterImage.png');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
